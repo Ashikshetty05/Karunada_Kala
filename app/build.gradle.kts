d7bbdbaa93e6a4116c0dac58c090a0b6e04ad751
Ashikshetty05 <ashikshetty102@gmail.com>
2026-05-19 01:20:30 +0530

    Initial commit - secure API key setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,7 @@
+import java.util.Properties
+
+val localProperties = Properties()
+localProperties.load(rootProject.file("local.properties").inputStream())
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
@@ -17,8 +21,8 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        manifestPlaceholders["googleApiKey"] = localProperties["GOOGLE_API_KEY"] as String
     }
 
     buildTypes {
